6f481494356b3202c80e0d09ab9771c137900bf7
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-05 14:38:14 +0300

    search error

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,14 +12,14 @@ module.exports.cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   // обработка простых запросов CORS
   if (allowedCors.includes(origin)) {
+    // res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Origin', '*');
   }
   // обработка предварительных запросов CORS
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.end();
   }
-
-  return next();
+  next();
 };

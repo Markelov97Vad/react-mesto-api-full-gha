30c28668166f796d92cd36d5ae948b05b9a64f49
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-05 14:53:29 +0300

    search error

--- backend/middlewares/errorHendler.js
+++ backend/middlewares/errorHendler.js
@@ -3,12 +3,12 @@ const { SERVER_ERROR_CODE } = require('../utils/codeStatus');
 module.exports.errorHendler = (err, req, res, next) => {
   const { statusCode = SERVER_ERROR_CODE, message } = err;
   res
-    .status(statusCode)
+    .status(err.statusCode)
     // .send({
     //   message: statusCode === SERVER_ERROR_CODE
     //     ? 'На сервере произошла ошибка'
     //     : message,
     // });
-    .send(err);
+    .send(err.message);
   next();
 };

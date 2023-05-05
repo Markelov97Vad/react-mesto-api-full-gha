bfaf31d84bc5da652a4f3a0b652ee6e720a9b124
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-05 16:13:27 +0300

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -15,6 +15,7 @@ module.exports.cors = (req, res, next) => {
     // res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Origin', '*');
   }
+  res.header('Access-Control-Allow-Origin', '*');
   // обработка предварительных запросов CORS
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);

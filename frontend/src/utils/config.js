54d3bdcacd87de8d9a0cdeae419abcf3e31f1edb
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-07-20 20:08:33 +0300

    refact: deploy a new VM

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-// const BASE_URL = 'https://api.mesto.marsello.nomoredomains.monster';
 const BASE_URL = 'http://localhost:3001';
+// const BASE_URL = 'https://api.mesto.marsello.nomoredomains.monster';
 
 export default BASE_URL;
